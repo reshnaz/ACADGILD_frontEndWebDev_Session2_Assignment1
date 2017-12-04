5fb2e0d883cf80dbc4b7fc3d4362844e537fb7f1
Reshma Hussain <reshnaz91@gmail.com>
2017-12-04 15:43:03 +0530

    Commit

---
+++ src/function.js
@@ -0,0 +1,2 @@
+var authorCount = function (authName, count) { document.getElementById("para").innerHTML = "The author, " + authName + " has authored " + count + " books."; };
+//# sourceMappingURL=S2A1.js.map
